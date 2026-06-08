a29ddec4f93aa95ca76e6c378f76fd6d2cb52f6c
chetankumawat13 <kumawatchetan2025@gmail.com>
2026-06-08 22:56:28 +0530

    feat: update GSAP animations for improved box movement effect

--- day-1/src/main.js
+++ day-1/src/main.js
@@ -1,10 +1,21 @@
+
 import './style.css'
 import gsap from 'gsap'
 
 const box = document.querySelector('.box')
 
-gsap.to(box,{
-  x:400,
-  duration:0.6,
-  delay:1
+// gsap.to(box,{
+//   x:400,
+//   duration:0.6,
+//   delay:1
+// })
+
+gsap.set(box,{
+  x:300
+})
+
+gsap.from(box,{
+  x:800,
+  duration:1,
+  delay:0.4
 })
